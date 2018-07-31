9799ef56a2b271c7ae9ac5f13fafec9736d66a19
radha kalal <radhakalal49@gmail.com>
2018-07-31 20:39:51 +0530

    static resoucs loding done but little problem

--- onlineshopping/src/main/java/com/rk/shopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/rk/shopping/controller/PageController.java
@@ -1,7 +1,9 @@
 package com.rk.shopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -15,4 +17,34 @@ public	ModelAndView index(){
 		return mv;
 		
 	}
+	
+
+	/*@RequestMapping(value={"/test"})
+public	ModelAndView test(@RequestParam (value="gretting",required=false) String gretting){
+		if(gretting==null){
+			gretting="welcome to shopping in india";
+		}
+		
+		
+		
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("gretting",gretting);
+		return mv;
+		
+	}
+	*/
+	
+	@RequestMapping(value="/test/{gretting}")
+	public	ModelAndView test( @PathVariable (value="gretting",required=false) String gretting){
+			if(gretting==null){
+				gretting="welcome to shopping in india";
+			}
+			
+			
+			
+			ModelAndView mv=new ModelAndView("page");
+			mv.addObject("gretting",gretting);
+			return mv;
+			
+		}
 }
